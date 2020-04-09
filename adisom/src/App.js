239fc20e2b1fd51272b8982153ee6194790859b1
Priyanshu Tyagi <priyanshutyagi@Priyanshus-MacBook-Air.local>
2020-04-09 14:48:46 +0530

    Header background image added

--- adisom/src/App.js
+++ adisom/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/NavigationBar/NavigationBar.component';
+import Header from './components/Header/Header.component';
 
 export default class App extends Component {
       constructor(props) {
@@ -13,10 +14,15 @@ export default class App extends Component {
         return(<NavigationBar /> )
       }
 
+      renderHeader = () => {
+        return(<Header /> )
+      }
+
       render() {
         return(
           <div> 
             {this.renderNavBar()}
+            {this.renderHeader()}
           </div> 
         )
       }
---
+++ adisom/src/components/Header/Header.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <span class="header__text-box">
+                <h1 class="heading-primary">
+                    <span class="heading-primary--main">ADISOM</span>
+                    <span class="heading-primary--sub">is where life happens</span>
+                </h1>
+            </span>
+    </div>
+);
+
+export default Header;
